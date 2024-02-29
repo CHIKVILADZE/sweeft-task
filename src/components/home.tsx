2bdf05f34a4eccc791126e82ec966119f9327535
Giorgi Chikviladze <chikviladze555@yahoo.com>
2024-02-29 21:20:07 +0400

    add infinity scroll functional in home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -18,24 +18,50 @@ const API_URL = `https://api.unsplash.com/photos?client_id=${API_KEY}`;
 const HomeComponent: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [photos, setPhotos] = useState<Photo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     fetchPhotos();
   }, []);
 
-  const fetchPhotos = async (term?: string) => {
-    const url = term ? `${API_URL}&query=${term}` : API_URL;
+  const fetchPhotos = async (term?: string, pageNum: number = page) => {
+    const url = term
+      ? `${API_URL}&query=${term}&page=${pageNum}`
+      : `${API_URL}&page=${pageNum}`;
     try {
       const response = await axios.get<Photo[]>(url);
-      setPhotos(response.data);
-      console.log('responseeee', response.data[1]);
+      if (pageNum === 1) {
+        setPhotos(response.data);
+      } else {
+        setPhotos((prevPhotos) => [...prevPhotos, ...response.data]);
+      }
+      setIsLoading(false); // Reset isLoading after fetching photos
     } catch (error) {
       console.error('Error fetching photos:', error);
     }
   };
 
+  const handleScroll = () => {
+    const scrollThreshold = 100;
+    const scrolledToBottom =
+      window.innerHeight + window.scrollY >=
+      document.documentElement.scrollHeight - scrollThreshold;
+
+    if (scrolledToBottom && !isLoading) {
+      setIsLoading(true);
+      setPage((prevPage) => prevPage + 1);
+      fetchPhotos(searchTerm, page + 1);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [isLoading]);
+
   return (
-    <div>
+    <div style={{ height: '100vh' }}>
       <div>
         <input
           type="text"
@@ -52,7 +78,6 @@ const HomeComponent: React.FC = () => {
         {photos.map((photo) => (
           <div key={photo.id}>
             <img src={photo.urls.regular} alt={photo.id} />
-            <button>View Details</button>
           </div>
         ))}
       </div>
