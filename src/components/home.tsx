2461b037e7f488ffc4da911d566be9627c5d18c7
Giorgi Chikviladze <chikviladze555@yahoo.com>
2024-03-01 13:44:27 +0400

    fetch statistics of downoalad and views

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -12,35 +12,32 @@ interface Photo {
   views: number;
 }
 
-const API_KEY = 'uptUNlgKP2lnP_twCh_zQREVyC49F96JabqiACl4QRo';
-const API_URL = `https://api.unsplash.com/photos?client_id=${API_KEY}`;
+const apiKey = process.env.REACT_APP_API_KEY;
+const API_URL = `https://api.unsplash.com/photos?client_id=${apiKey}`;
+console.log(apiKey);
 
 const HomeComponent: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [photos, setPhotos] = useState<Photo[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
+  const [searchHistory, setSearchHistory] = useState<string[]>([]);
+  const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
 
-  useEffect(() => {
-    fetchPhotos();
-  }, []);
-
-  const fetchPhotos = async (term?: string, pageNum: number = page) => {
-    const url = term
-      ? `${API_URL}&query=${term}&page=${pageNum}`
-      : `${API_URL}&page=${pageNum}`;
+  const fetchPopularPhotos = async () => {
+    const url = `${API_URL}&order_by=popular`;
     try {
       const response = await axios.get<Photo[]>(url);
-      if (pageNum === 1) {
-        setPhotos(response.data);
-      } else {
-        setPhotos((prevPhotos) => [...prevPhotos, ...response.data]);
-      }
-      setIsLoading(false); // Reset isLoading after fetching photos
+      setPhotos(response.data);
+      setIsLoading(false);
     } catch (error) {
-      console.error('Error fetching photos:', error);
+      console.error('Error fetching popular photos:', error);
     }
   };
+
+  useEffect(() => {
+    fetchPopularPhotos();
+  }, []);
 
   const handleScroll = () => {
     const scrollThreshold = 100;
@@ -51,7 +48,7 @@ const HomeComponent: React.FC = () => {
     if (scrolledToBottom && !isLoading) {
       setIsLoading(true);
       setPage((prevPage) => prevPage + 1);
-      fetchPhotos(searchTerm, page + 1);
+      fetchPopularPhotos();
     }
   };
 
@@ -60,20 +57,69 @@ const HomeComponent: React.FC = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [isLoading]);
 
+  const handleSearch = () => {
+    if (searchTerm.trim() !== '') {
+      fetchPopularPhotos();
+      setSearchHistory((prev) => [...prev, searchTerm]);
+    }
+  };
+
+  const fetchPhotoStatistics = async (photoId: string) => {
+    const url = `https://api.unsplash.com/photos/${photoId}/statistics?client_id=${apiKey}`;
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching photo statistics:', error);
+      return null;
+    }
+  };
+
+  const handleSearchHistoryClick = (term: string) => {
+    setSearchTerm(term);
+    fetchPopularPhotos();
+  };
+
+  const handleImageClick = async (photo: Photo) => {
+    setSelectedPhoto(photo);
+    const statistics = await fetchPhotoStatistics(photo.id);
+    if (statistics) {
+      console.log('Views:', statistics.views.total);
+      console.log('Downloads:', statistics.downloads.total);
+    }
+  };
+
+  const closeModal = () => {
+    setSelectedPhoto(null);
+  };
+
+  console.log('selectedPhoto', selectedPhoto);
+
   return (
     <div style={{ height: '100vh' }}>
       <div>
         <input
           type="text"
           value={searchTerm}
+          className="border-2"
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search..."
         />
+        <button className="border-2" onClick={handleSearch}>
+          Search
+        </button>
       </div>
       <div>
         <h2>Search History</h2>
+        <ul>
+          {searchHistory.map((term, index) => (
+            <li key={index} onClick={() => handleSearchHistoryClick(term)}>
+              {term}
+            </li>
+          ))}
+        </ul>
       </div>
-      <div className="border-2 border-green-700  flex flex-row flex-wrap gap-4 ">
+      <div className="border-2 border-green-700  flex mt-4 ">
         <h2 className="text-3xl font-bold underline">Popular Images</h2>
         {photos.map((photo) => (
           <div key={photo.id} className="">
@@ -82,10 +128,21 @@ const HomeComponent: React.FC = () => {
               alt={photo.id}
               width={200}
               height={200}
+              onClick={() => handleImageClick(photo)}
             />
           </div>
         ))}
       </div>
+      {selectedPhoto && (
+        <div className="modal  border-4 w-1/2 h-1/2 border-red-500">
+          <div className="modal-content w-1/2 h-1/2 border-4 border-green-500">
+            <img src={selectedPhoto.urls.full} alt={selectedPhoto.id} />
+            <p>Views: {selectedPhoto.views}</p>
+            <p>Likes: {selectedPhoto.likes}</p>
+            <button onClick={closeModal}>Close</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
